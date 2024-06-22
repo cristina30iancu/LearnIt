4e9b257493e3ab93103c11fae9a94314c4c1c95f
Cristina Iancu <cristina30.iancu@gmail.com>
2024-06-22 16:18:32 +0300

    ok

--- backend/server/rute.js
+++ backend/server/rute.js
@@ -1,10 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const { getQuizByIdAndSubject, getSingleUserAnswer, getQuizBySubject, createQuiz, updateQuiz, deleteQuiz, getAnswersOfUser, submitQuiz } = require('./controllers/quizController');
+const {  getSingleUserAnswer, getQuizBySubject, createQuiz, updateQuiz, deleteQuiz, getAnswersOfUser, submitQuiz } = require('./controllers/quizController');
 const { protect } = require('../middleware/authMiddleware');
-router.get('/quiz/:id/:subject', getQuizByIdAndSubject);
-
-router.get('/quiz/:subject', getQuizBySubject);
+router.put('/quiz/', getQuizBySubject);
 router.post('/quiz', createQuiz);
 router.put('/quiz/:id', updateQuiz);
 router.delete('/quiz/:id', deleteQuiz);
